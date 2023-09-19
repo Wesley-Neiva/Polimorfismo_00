1caabe2f1babd9e5b6fda4909feba6f219c869ab
Wesley <yelsew84@gmail.com>
2023-09-18 21:37:13 -0300

    Programa final

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,42 +22,36 @@ public class Program {
 		System.out.println("Enter the number of employees:");
 		int n = sc.nextInt();
 		
-		for(int i = 1; i<=n; i++) {
-			System.out.println("Employee #" + i + " data:");
-			sc.nextLine();
-			System.out.print("Outsourced (y/n)? ");
-			char ch = sc.next().charAt(0);
-			sc.nextLine();
-			
-			if(ch == 'n') {
+			for(int i = 1; i<=n; i++) {
+				System.out.println("Employee #" + i + " data:");
+				sc.nextLine();
+				System.out.print("Outsourced (y/n)? ");
+				char ch = sc.next().charAt(0);
+				sc.nextLine();
 				System.out.print("Name: ");
 				String name = sc.nextLine();
 				System.out.print("Hours: ");
 				int hours = sc.nextInt();
 				System.out.print("Value per hour: ");
 				double valuePerHour = sc.nextDouble();
+				
+			if(ch == 'n') {
 				outsourced.add(new Employee(name, hours, valuePerHour));
 				System.out.println();
-									
+												
 				
-			} if(ch != 'n') {
-				System.out.print("Name: ");
-				String name = sc.nextLine();
-				System.out.print("Hours: ");
-				int hours = sc.nextInt();
-				System.out.print("Value per hour: ");
-				double valuePerHour = sc.nextDouble();	
+			} else {
 				System.out.print("Addition charge: ");
 				double additional = sc.nextDouble();
 				outsourced.add(new OutsourcedEmployee(name, hours, valuePerHour, additional));
 				System.out.println();
-								
+						
 			}
 						
 		}
-		System.out.println();
-		System.out.println("PAYMENTS: ");
-		for(Employee emp: outsourced) {
+			System.out.println();
+			System.out.println("PAYMENTS: ");
+			for(Employee emp: outsourced) {
 			System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
 		}
 			
